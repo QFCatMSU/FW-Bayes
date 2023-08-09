d005f3500f8feb63014eda07127e13589fe83650
Charlie <belinsky@msu.edu>
2023-08-09 12:57:03 -0400

    Updated software installation and move to its own folder

---
+++ installation/Installation_Test.R
@@ -0,0 +1,23 @@
+library(cmdstanr)
+library(bayesplot)
+library(ggqfc)
+
+file <- file.path(cmdstan_path(), "examples", "bernoulli", "bernoulli.stan")
+mod <- cmdstan_model(file)
+
+# names correspond to the data block in the Stan program
+data_list <- list(N = 10, y = c(0,1,0,0,0,0,0,0,0,1))
+
+fit <- mod$sample(
+  data = data_list,
+  seed = 123,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 500 # print update every 500 iters
+)
+
+# draws_arr <- fit$draws() # or format="array"
+# str(draws_arr)
+
+plot1 = mcmc_hist(fit$draws("theta")) + theme_qfc()
+plot(plot1)
