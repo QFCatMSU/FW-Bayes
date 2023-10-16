94a29a1b0ab718360fb7ed6264a0a3093f67593d
Chris Cahill <christopherfishcahill@gmail.com>
2023-10-15 22:10:19 -0400

    fix lec9, schools

--- week6/R/schools.r
+++ week6/R/schools.r
@@ -1,8 +1,11 @@
+#--------------------------
+# simulating Devil's funnel in R:
 set.seed(1)
 nobs <- 1000
 theta_i <- rnorm(nobs, 0, 1) # local parameter
 v <- rnorm(nobs, 0, exp(theta_i)) # global parameter 
 plot(theta_i ~ v, main = "visualizing the devil's funnel")
+#--------------------------
 
 library("bayesplot")
 library("ggplot2")
@@ -17,6 +20,7 @@ schools_dat <- data.frame(
 
 schools_dat %>%
     ggplot(aes(x = y, y = 1:J)) + 
+    ylab("school") + xlab("standardized test score") + 
     geom_point() 
 
 mod_cp <- cmdstan_model("week6/src/schools_cp.stan")
@@ -43,12 +47,11 @@ post <- fit$draws(format = "df") # extract draws x variables data frame
 np <- nuts_params(fit)
 
 color_scheme_set("darkgray")
-div_style <- scatter_style_np(div_color = "red",  div_size = 4)
+mcmc_parcoord(post, np = np)
 
 mcmc_pairs(post, np = np, pars = c("mu","tau","theta[1]"),
            off_diag_args = list(size = 0.75), 
            np_style = pairs_style_np(div_color = "firebrick", div_size = 2))
-?mcmc_pairs
 
 #--------------------------------------------------
 # non centered parameterization
